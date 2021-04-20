8d147f40df3d0fe17a26bd1a1e8ee02fc5137eaf
weiqiang <weiqiang_1989@126.com>
2021-04-20 14:24:28 +0800

    Udpate ProductFlavors

--- CompleteProject/ProductFlavors/app/src/main/java/pri/weiqiang/study/productflavors/MainActivity.java
+++ CompleteProject/ProductFlavors/app/src/main/java/pri/weiqiang/study/productflavors/MainActivity.java
@@ -2,6 +2,9 @@ package pri.weiqiang.study.productflavors;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import pri.weiqiang.study.common.Utils;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView mTvInfo = findViewById(R.id.tv_info);
+        mTvInfo.setText(Utils.getInfo());
     }
 }
---
+++ CompleteProject/ProductFlavors/common/src/main/java/pri/weiqiang/study/common/Utils.java
@@ -0,0 +1,8 @@
+//package pri.weiqiang.study.common;
+//
+//public class Utils {
+//    public static String  getInfo(){
+//       return "I am from demo001!";
+////        return "I am from demo002!";
+//    }
+//}
